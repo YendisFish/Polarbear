0d870dd5d23ae5398945801a66c59e8a32fe91e9
YendisFish <brintonsid@gmail.com>
2023-10-26 17:25:39 -0600

    QOL features added to the scripting classes

--- Src/Scripting/Query.cs
+++ Src/Scripting/Query.cs
@@ -8,78 +8,46 @@ public class Query<T> where T : Enterable
 {
     internal T obj { get; set; }
     public PolarbearDB db { get; set; }
-    
-    internal List<QueryClause> query { get; set; }
-    internal List<T> outcome { get; set; }
+    public List<T> Outcome { get; set; }
 
     public Query(List<T> baseObjs, PolarbearDB parentDb)
     {
         db = parentDb;
-        query = new();
-        outcome = baseObjs;
+        Outcome = baseObjs;
     }
 
-    public List<T> Result()
+    public void Where(Func<T, bool> condition, T? template = null,
+        params string[] toInclude) // query the database and live compare here
     {
-        return outcome;
-    }
+#warning Polarbear.Scripting.Query.Where() is experimental! Please be carefule while using this function!
 
-    public void Where(Func<T, bool> condition, T? template = null, params string[] toInclude) // query the database and live compare here
-    {
-        #warning Polarbear.Scripting.Query.Where() is experimental! Please be carefule while using this function!
-        
         List<T> newOutcome = new();
 
-        if(template is not null)
+        if (template is not null)
         {
             IEnumerable<PropertyInfo> props = typeof(T).GetProperties().Where(x => toInclude.Contains(x.Name));
             IEnumerable<FieldInfo> fields = typeof(T).GetFields().Where(x => toInclude.Contains(x.Name));
-        
-            foreach(PropertyInfo prop in props)
+
+            foreach (PropertyInfo prop in props)
             {
                 object? o = prop.GetValue(template);
                 string stringRep = JsonConvert.SerializeObject(o);
-            
-                if(!db.reverseLookup.ContainsKey(stringRep) || !db.reverseLookup[stringRep].ContainsKey(typeof(T).Name))
+
+                if (!db.reverseLookup.ContainsKey(stringRep) ||
+                    !db.reverseLookup[stringRep].ContainsKey(typeof(T).Name))
                 {
                     break;
                 }
 
                 CompareLogic logic = new();
                 logic.Config.MembersToInclude = toInclude.ToList();
-            
-                for(int i = 0; i < db.reverseLookup[stringRep][typeof(T).Name].Count; i++)
-                {
-                    ComparisonResult result = logic.Compare(template, db.reverseLookup[stringRep][typeof(T).Name][i]);
-                    if(result.AreEqual)
-                    {
-                        if(condition(template))
-                        {
-                            newOutcome.Add((T)db.reverseLookup[stringRep][typeof(T).Name][i]);
-                        }
-                    }
-                }
-            }
-        
-            foreach(FieldInfo prop in fields)
-            {
-                object? o = prop.GetValue(template);
-                string stringRep = JsonConvert.SerializeObject(o);
-            
-                if(!db.reverseLookup.ContainsKey(stringRep) || !db.reverseLookup[stringRep].ContainsKey(typeof(T).Name))
-                {
-                    break;
-                }
 
-                CompareLogic logic = new();
-                logic.Config.MembersToInclude = toInclude.ToList();
-            
-                for(int i = 0; i < db.reverseLookup[stringRep][typeof(T).Name].Count; i++)
+                for (int i = 0; i < db.reverseLookup[stringRep][typeof(T).Name].Count; i++)
                 {
                     ComparisonResult result = logic.Compare(template, db.reverseLookup[stringRep][typeof(T).Name][i]);
-                    if(result.AreEqual)
+                    if (result.AreEqual)
                     {
-                        if(condition(template))
+                        if (condition(template))
                         {
                             newOutcome.Add((T)db.reverseLookup[stringRep][typeof(T).Name][i]);
                         }
@@ -87,56 +55,60 @@ public class Query<T> where T : Enterable
                 }
             }
 
-            outcome = newOutcome;
-        } else {
+            foreach (FieldInfo prop in fields)
+            {
+                object? o = prop.GetValue(template);
+                string stringRep = JsonConvert.SerializeObject(o);
+
+                if (!db.reverseLookup.ContainsKey(stringRep) ||
+                    !db.reverseLookup[stringRep].ContainsKey(typeof(T).Name))
+                {
+                    break;
+                }
+
+                CompareLogic logic = new();
+                logic.Config.MembersToInclude = toInclude.ToList();
+
+                for (int i = 0; i < db.reverseLookup[stringRep][typeof(T).Name].Count; i++)
+                {
+                    ComparisonResult result = logic.Compare(template, db.reverseLookup[stringRep][typeof(T).Name][i]);
+                    if (result.AreEqual)
+                    {
+                        if (condition(template))
+                        {
+                            newOutcome.Add((T)db.reverseLookup[stringRep][typeof(T).Name][i]);
+                        }
+                    }
+                }
+            }
+
+            Outcome = newOutcome;
+        }
+        else
+        {
             IEnumerable<PropertyInfo> props = typeof(T).GetProperties().Where(x => toInclude.Contains(x.Name));
             IEnumerable<FieldInfo> fields = typeof(T).GetFields().Where(x => toInclude.Contains(x.Name));
-        
-            foreach(PropertyInfo prop in props)
+
+            foreach (PropertyInfo prop in props)
             {
                 object? o = prop.GetValue(template);
                 string stringRep = JsonConvert.SerializeObject(o);
-            
-                if(!db.reverseLookup.ContainsKey(stringRep) || !db.reverseLookup[stringRep].ContainsKey(typeof(T).Name))
+
+                if (!db.reverseLookup.ContainsKey(stringRep) ||
+                    !db.reverseLookup[stringRep].ContainsKey(typeof(T).Name))
                 {
                     break;
                 }
 
                 CompareLogic logic = new();
                 logic.Config.MembersToInclude = toInclude.ToList();
-            
-                for(int i = 0; i < db.reverseLookup[stringRep][typeof(T).Name].Count; i++)
-                {
-                    ComparisonResult result = logic.Compare(template, db.reverseLookup[stringRep][typeof(T).Name][i]);
-                    if(result.AreEqual)
-                    {
-                        if(condition(obj))
-                        {
-                            newOutcome.Add((T)db.reverseLookup[stringRep][typeof(T).Name][i]);
-                        }
-                    }
-                }
-            }
-        
-            foreach(FieldInfo prop in fields)
-            {
-                object? o = prop.GetValue(template);
-                string stringRep = JsonConvert.SerializeObject(o);
-            
-                if(!db.reverseLookup.ContainsKey(stringRep) || !db.reverseLookup[stringRep].ContainsKey(typeof(T).Name))
-                {
-                    break;
-                }
 
-                CompareLogic logic = new();
-                logic.Config.MembersToInclude = toInclude.ToList();
-            
-                for(int i = 0; i < db.reverseLookup[stringRep][typeof(T).Name].Count; i++)
+                for (int i = 0; i < db.reverseLookup[stringRep][typeof(T).Name].Count; i++)
                 {
                     ComparisonResult result = logic.Compare(template, db.reverseLookup[stringRep][typeof(T).Name][i]);
-                    if(result.AreEqual)
+                    if (result.AreEqual)
                     {
-                        if(condition(obj))
+                        if (condition(obj))
                         {
                             newOutcome.Add((T)db.reverseLookup[stringRep][typeof(T).Name][i]);
                         }
@@ -144,9 +116,49 @@ public class Query<T> where T : Enterable
                 }
             }
 
-            outcome = newOutcome;
+            foreach (FieldInfo prop in fields)
+            {
+                object? o = prop.GetValue(template);
+                string stringRep = JsonConvert.SerializeObject(o);
+
+                if (!db.reverseLookup.ContainsKey(stringRep) ||
+                    !db.reverseLookup[stringRep].ContainsKey(typeof(T).Name))
+                {
+                    break;
+                }
+
+                CompareLogic logic = new();
+                logic.Config.MembersToInclude = toInclude.ToList();
+
+                for (int i = 0; i < db.reverseLookup[stringRep][typeof(T).Name].Count; i++)
+                {
+                    ComparisonResult result = logic.Compare(template, db.reverseLookup[stringRep][typeof(T).Name][i]);
+                    if (result.AreEqual)
+                    {
+                        if (condition(obj))
+                        {
+                            newOutcome.Add((T)db.reverseLookup[stringRep][typeof(T).Name][i]);
+                        }
+                    }
+                }
+            }
+
+            Outcome = newOutcome;
         }
     }
-}
 
-internal record QueryClause();
+    public void WhereOutcome(Func<T, bool> condition)
+    {
+        List<T> newOutcome = new();
+
+        foreach(T element in Outcome)
+        {
+            if(condition(element))
+            {
+                newOutcome.Add(element);
+            }
+        }
+
+        Outcome = newOutcome;
+    }
+}
